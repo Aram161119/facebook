ab602cf7f82d661aa913491f5e9b210b28682273
Aram161119 <hrayryan77@gmail.com>
2021-08-14 08:50:26 -0700

    ghfhgfhgf

---
+++ index.php
@@ -0,0 +1,45 @@
+<?php 
+/*	class A {
+		public $first_name = '';
+		public function __construct($first_name) {
+			$this->first_name = $first_name;
+		}
+	}
+	
+	class B extends A
+	{
+		
+		public function __construct($age, $first_name)
+		{
+			
+		}
+	}
+	echo $a->x;*/
+
+/*
+abstract class A {
+	public function getUser() {
+		return 'John Smith';
+	}
+	abstract public function getX($x);
+}
+
+class B extends A {
+	public function getX($x) {
+		return $x;
+	}
+}
+
+$x1 = new B();
+echo $x1->getUser();
+
+*/
+
+
+
+
+
+
+
+
+ ?>
